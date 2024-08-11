521dbc95c40ef36ee2c86d7664bbc1fbf2c63c2e
Moein Moeinnia <Moeinnia@protonmail.com>
2024-08-11 21:19:41 +0330

    chore: deploy mi

--- docker/bash.sh
+++ docker/bash.sh
@@ -3,3 +3,10 @@ docker stack deploy --compose-file docker-compose.yml hamyarchat
 
 #service status
 docker stack services hamyarchat
+
+
+#mi
+docker run -p 9000:9000 -p 9001:9001  --name minio     -e "MINIO_ROOT_USER=admin"     -e "MINIO_ROOT_PASSWORD=admin123"     quay.io/minio/minio:RELEASE.2023-11-01T01-57-10Z-cpuv1 server /data --console-address ":9001"
+
+# for stream
+docker run -p 9000:9000 -p 9001:9001 -d --name minio --restart unless-stopped -e "MINIO_ROOT_USER=admin" -e "MINIO_ROOT_PASSWORD=admin123" minio/minio server /data --console-address ":9001"
